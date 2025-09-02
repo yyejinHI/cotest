db65482691610217f204da76eb0988c8214f98c7
yyejinHI <yyejin_heo@naver.com>
2025-09-02 13:19:14 +0900

    [Bronze V] Title: 윤년, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2753. 윤년/윤년.py
+++ 백준/Bronze/2753. 윤년/윤년.py
@@ -1,6 +1,12 @@
 y = int(input())
 
-if (y % 4 == 0 and y % 100 != 0) or y % 400 == 0:
-    print(1)
-else:
-    print(0)
+# if (y % 4 == 0 and y % 100 != 0) or y % 400 == 0:
+#     print(1)
+# else:
+#     print(0)
+
+#한 줄로 쓰기 
+# print(1 if (y % 4 == 0 and y % 100 != 0) or y % 400 == 0 else 0 )
+
+#불리언 이용하기 
+print(int((y % 4 == 0 and y % 100 != 0) or y % 400 == 0))
